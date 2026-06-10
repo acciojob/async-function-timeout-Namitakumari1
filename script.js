c5d165f4ab5ea39c939ce473917444296c543334
Namitakumari1 <137058713+Namitakumari1@users.noreply.github.com>
2026-06-10 17:52:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const textInput = document.getElementById("text");
+const delayInput = document.getElementById("delay");
+const btnInput = document.getElementById("btn");
+const outputDiv = document.getElementById("output");
+
+function wait(ms){
+	return new Promise((resolve) => {
+		setTimeout(resolve,ms);
+	});
+}
+
+async function displayMessage(){
+	const text = textInput.value;
+	const delay = Number(delayInput.value);
+
+	outputDiv.textContent = "";
+
+	await wait(delay);
+
+	outputDiv.textContent = text;
+}
+btnInput.addEventListener("click", displayMessage);
